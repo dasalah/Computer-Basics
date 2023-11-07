cfa971cf482fa68aa74fee54031c8d6f7199f4a1
B A G H E R I <B A G H E R I@SALAH>
2023-11-07 14:22:36 +0330

    Add project files.

---
+++ Computer Basics/Computer Basics.cpp
@@ -0,0 +1,18 @@
+// Computer Basics.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	int a, s = 0;
+	while(1){
+		cin >> a;
+		if (a == 0) break;
+		s += a
+	}
+	cout << 5;
+	return 0;
+
+}
---
+++ Computer Basics/continue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, x;
+	cout << "enter a";
+	cin >> a;
+	while (1)
+		if (a > 0)
+		x += a;
+		else continue;
+
+
+
+
+}
